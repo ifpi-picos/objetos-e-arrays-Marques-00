68cde6e0b3281dc3880f80381a038b539d2559b4
Marques-00 <amandafmarques00@gmail.com>
2024-07-19 15:17:20 -0300

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,7 @@
+//Crie um Objeto livro com as propriedades titulo, autor e anoPublicacao. Imprima o título e o autor do livro.
+const livro = {
+    titulo :"jantar secreto",
+    autor : "Rafael montes",
+    anopublicacao :2016,
+
+}; console.log (livro.titulo, livro.autor)
---
+++ index4.js
@@ -0,0 +1,11 @@
+/* Crie um Objeto produto com as propriedades nome,preco e quantidade. Em
+seguida, calcule e imprima o valor total do estoque (preço * quantidade). */
+
+const produto = {
+    nome : "arroz",
+    preco: 5,
+    quantidade : 10,
+  } ;
+    
+  const valortotal = produto.preco * produto.quantidade
+  console.log(valortotal)
