bbc3ec46f27f2281022aa1471aa5cd7bd3ae2a75
Marques-00 <amandafmarques00@gmail.com>
2024-07-20 15:54:48 -0300

    Add files via upload

---
+++ index11.js
@@ -0,0 +1,5 @@
+//Dado o array let cidades = ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Salvador', 'Fortaleza'], use o método splice() para remover as cidades Belo Horizonte e Salvador do array.
+const cidades =['Sao Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Salvador', 'Fortaleza']
+const cidadesRemovidas =cidades.splice (2,2)
+console.log (cidades)
+  //metodo splice apartir do indice dois remove dois, contando dele mesmo.
---
+++ index9.js
@@ -0,0 +1,5 @@
+//Dado o array let frutas = ['maçã', 'banana'] e o array let maisFrutas = ['laranja', 'pera'], use o método concat() para combinar os dois arrays em um novo array e exiba o resultado.
+const frutas1 = ['maça, banana']
+const maisFrutas2 = ['laranja, pera']
+const todasAsFrutas = frutas1.concat (maisFrutas2)
+console.log (todasAsFrutas)
