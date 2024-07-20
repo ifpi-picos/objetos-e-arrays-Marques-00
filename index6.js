88c7a2109b11e6cab6ab31ef909229c39a555b14
Marques-00 <amandafmarques00@gmail.com>
2024-07-20 11:32:40 -0300

    Add files via upload

---
+++ index6.js
@@ -0,0 +1,4 @@
+//Crie um array com os elementos [5, 10, 15, 20]. Use o método pop() para remover o último elemento do array. Exiba o array resultante e o elemento removido.
+const elementos =[5,10,15,20];
+const elementosRemovida = elementos.pop ();
+console.log (elementosRemovida);
---
+++ index7.js
@@ -0,0 +1,4 @@
+//Dado o array let numeros = [1, 2, 3, 4, 5], use o método shift() para remover o primeiro elemento do array. Exiba o array resultante e o elemento removido.
+const numeros = [1,2,3,4,5]
+const primeiroElemento = numeros.shift();
+console.log ("array resultante", numeros, primeiroElemento)
